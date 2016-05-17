e5be4d8963eb057e7d8e4856b8242b61f545c301
Richard Feldman <richard.t.feldman@gmail.com>
2016-05-17 11:19:41 -0700

    Remove some unnecessary stuff from example.js

--- example/example.js
+++ example/example.js
@@ -1,10 +1,8 @@
 var Supervisor = require("elm-web-workers");
 var path = require("path");
 var elmPath = path.join(__dirname, "Elm.js");
-var http = require("http");
 
 var supervisor = new Supervisor(elmPath, "Example");
-
 
 supervisor.on("emit", function(msg) {
   console.log("[supervisor]:", msg);
@@ -29,10 +27,5 @@ supervisor.send({msgType: "spawn", data: "" + Math.random()});
 
 
 setInterval(function() {
-  console.log("sending echoViaWorker message to supervisor...")
   supervisor.send({msgType: "echoViaWorker", data: "5"});
-  console.log("sent message to supervisor without crashing")
 }, 2000);
-
-// Spin up a server just to prevent exiting
-http.createServer(function(request, response) { response.end(); }).listen(8090);
